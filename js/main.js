62d439722d704d8202c1e4e3df3481fcb705ea5c
Sean Oh <ohsiwon@gmail.com>
2014-01-07 13:42:58 -0500

    Many updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+$(document).ready(function(){
 
+    $(".various").fancybox({
+        maxWidth    : 960,
+        maxHeight   : 540,
+        fitToView   : false,
+        width       : '90%',
+        height      : '90%',
+        autoSize    : true,
+        closeClick  : false,
+        // openEffect  : 'none',
+        // closeEffect : 'none'
+    });
+
+
+    var $nav_selected = $('#nav :nth-child(1)'), $nav_id;
+ 	$('.nav_item').click(function(i){
+ 		if(!$(this).hasClass('selected')){
+ 			$(this).addClass('selected');
+ 			$nav_selected.removeClass('selected');
+ 			$nav_selected = $(this);
+
+ 			$nav_id = $(this).attr('id');
+ 			$('#content ul li').each(function(i,v){
+ 				if($nav_id == i){
+ 					$(v).removeClass('hidden');
+ 				}
+ 				else{
+ 					$(v).addClass('hidden');
+ 				}
+ 			});
+ 		}
+ 	});
+
+});
